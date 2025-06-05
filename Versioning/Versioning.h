fdf8cc407d4a013df92b6d041b98ba5923adef23
andei_buite <andei.buite@outlook.com>
2025-06-05 19:20:43 +0800

    Initial Commit

---
+++ Versioning/Versioning.h
@@ -0,0 +1,18 @@
+//
+//  Versioning.h
+//  Versioning
+//
+//  Created by Andei Buite on 2025/06/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Versioning.
+FOUNDATION_EXPORT double VersioningVersionNumber;
+
+//! Project version string for Versioning.
+FOUNDATION_EXPORT const unsigned char VersioningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Versioning/PublicHeader.h>
+
+
---
+++ VersioningTests/VersioningTests.swift
@@ -0,0 +1,17 @@
+//
+//  VersioningTests.swift
+//  VersioningTests
+//
+//  Created by Andei Buite on 2025/06/05.
+//
+
+import Testing
+@testable import Versioning
+
+struct VersioningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
